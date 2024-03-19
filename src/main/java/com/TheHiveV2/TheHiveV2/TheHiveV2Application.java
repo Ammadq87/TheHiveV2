fa7a6874aaeac41815427067a278aa8c598b947c
Ammad <ammadq87@my.yorku.ca>
2024-03-19 18:08:21 -0400

    V2 Initialization

---
+++ src/main/java/com/TheHiveV2/TheHiveV2/TheHiveV2Application.java
@@ -0,0 +1,13 @@
+package com.TheHiveV2.TheHiveV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheHiveV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheHiveV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TheHiveV2/TheHiveV2/TheHiveV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TheHiveV2.TheHiveV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheHiveV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
